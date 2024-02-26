758bbf392ea81e324ce993f79e3be514f1ccf637
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-02-26 12:51:26 +0100

    add snack-8 using while loop

--- snack-8/main.js
+++ snack-8/main.js
@@ -1,10 +1,24 @@
 // FOR LOOP
 
-let input = prompt('inserisci un numero di 4 cifre');
+/* let input = prompt('inserisci un numero di 4 cifre');
 let sum = 0;
 for (let i = 0; i < input.length; i++) {
     const element = Number(input[i]);
-    sum += element
+    sum += element;
 }
 
-alert(`la somma dei numeri è : ${sum}`)
+alert(`la somma dei numeri è : ${sum}`); */
+
+// WHILE LOOP
+
+let input = prompt('inserisci un numero di 4 cifre');
+let sum = 0;
+
+let i = 0;
+while (i < input.length) {
+    const element = Number(input[i]);
+    sum += element;
+    i++;
+}
+
+alert(`la somma dei numeri è : ${sum}`);
