6b1e4031741ed3ffd50380ef4f913e91eff7e458
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-02-26 11:48:38 +0100

    add snack-5 using while loop

--- snack-5/main.js
+++ snack-5/main.js
@@ -1,6 +1,6 @@
 // FOR LOOP
 
-let array = [];
+/* let array = [];
 
 for (let i = 0; i < 6; i++) {
     const element = array[i];
@@ -11,9 +11,24 @@ for (let i = 0; i < 6; i++) {
     }
 }
 
+console.log(array); */
+
+
+// WHILE LOOP
+
+let array = [];
+let i = 0;
+while ( i < 6) {
+    const element = array[i];
+    let input = Number(prompt('Inserisci un numero'))
+    
+    if (!(input % 2 == 0)) {
+        array.push(input)
+    }
+
+    i++;
+}
+
 console.log(array);
-
-
-
 
 
