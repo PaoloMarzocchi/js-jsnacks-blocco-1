b0fd3330076d5f4e972de2f6da6c6f9fc782b839
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-02-26 11:18:19 +0100

    add snack-3 with while loop

--- snack-3/main.js
+++ snack-3/main.js
@@ -2,10 +2,19 @@ let input;
 let numbers = [];
 let sum = 0;
 
-for (let i = 1; i <= 10; i++) {
+/* for (let i = 1; i <= 10; i++) {
     input = Number(prompt(`Inserisci il numero per 10 volte (${i}) `));
     numbers.push(input)
     sum += input;
+}
+
+console.log(sum); */
+let i = 1;
+while ( i <= 10) {
+    input = Number(prompt(`Inserisci il numero per 10 volte (${i}) `));
+    numbers.push(input)
+    sum += input;
+    i++;
 }
 
 console.log(sum);
