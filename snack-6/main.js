d19d8ddc771a6424ff5df12a1e012d14e2761caa
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-02-26 12:27:08 +0100

    add snack-6 using while loop

--- snack-6/main.js
+++ snack-6/main.js
@@ -1,11 +1,26 @@
 // FOR LOOP
 
-let output =[];
+/* let output =[];
 let input = Number(prompt('inserisci un numero'));
 
 for (let i = 0; i < input; i++) {
     let cube = Math.pow(i,3);
     output.push(cube);
+}
+
+alert(`i cubi dei primi numeri fino a ${input} sono questi: ${output}`); */
+
+
+// WHILE LOOP
+
+let output =[];
+let input = Number(prompt('inserisci un numero'));
+
+let i = 0;
+while(i < input) {
+    let cube = Math.pow(i,3);
+    output.push(cube);
+    i++
 }
 
 alert(`i cubi dei primi numeri fino a ${input} sono questi: ${output}`);
